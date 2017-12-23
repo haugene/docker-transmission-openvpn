85b89dc0cc529b5c937981cd9edd97e7377852ad
himred <himred@gasmi.net>
2017-12-23 13:10:21 +0100

    Update stop.sh

--- transmission/stop.sh
+++ transmission/stop.sh
@@ -1,3 +1,17 @@
 #! /bin/sh
 
+# If custom-pre-stop.sh exists, run it
+if [ -x /config/custom-pre-stop.sh ]
+then
+   echo "Executing /config/custom-pre-stop.sh"
+   /config/custom-pre-stop.sh
+fi
+
 kill $(ps aux | grep transmission-daemon | grep -v grep | awk '{print $2}')
+
+# If custom-post-stop.sh exists, run it
+if [ -x /config/custom-post-stop.sh ]
+then
+   echo "Executing /config/custom-post-stop.sh"
+   /config/custom-post-stop.sh
+fi
