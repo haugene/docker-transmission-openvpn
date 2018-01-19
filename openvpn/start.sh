d6cc30e9f57da66d8f69efbb2936931bd25c46ce
e7d <michael@e7d.io>
2018-01-19 15:53:09 +0100

    Install UFW only if needed

--- openvpn/start.sh
+++ openvpn/start.sh
@@ -50,7 +50,11 @@ dockerize -template /etc/transmission/environment-variables.tmpl:/etc/transmissi
 TRANSMISSION_CONTROL_OPTS="--script-security 2 --up-delay --up /etc/transmission/start.sh --down /etc/transmission/stop.sh"
 
 if [ "true" = "$ENABLE_UFW" ]; then
-  apk add --no-cache ufw@testing
+  if [ ! -e ufw ]; then
+    echo "UFW not found, installing"
+    echo "@testing http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+    apk add --no-cache ip6tables ufw@testing
+  fi
 
   # Enable firewall
   echo "enabling firewall"
