fba288866bc74c050677f500a8d3d269fb85513f
himred <himred@gasmi.net>
2018-01-21 16:24:02 +0100

    Update stop.sh

--- transmission/stop.sh
+++ transmission/stop.sh
@@ -1,17 +1,19 @@
 #! /bin/sh
 
-# If custom-pre-stop.sh exists, run it
-if [ -x /config/custom-pre-stop.sh ]
+# If transmission-pre-stop.sh exists, run it
+if [ -x /scripts/transmission-pre-stop.sh ]
 then
-   echo "Executing /config/custom-pre-stop.sh"
-   /config/custom-pre-stop.sh
+   echo "Executing /scripts/transmission-pre-stop.sh"
+   /scripts/transmission-pre-stop.sh
+   echo "/scripts/transmission-pre-stop.sh returned $?"
 fi
 
 kill $(ps aux | grep transmission-daemon | grep -v grep | awk '{print $2}')
 
-# If custom-post-stop.sh exists, run it
-if [ -x /config/custom-post-stop.sh ]
+# If transmission-post-stop.sh exists, run it
+if [ -x /scripts/transmission-post-stop.sh ]
 then
-   echo "Executing /config/custom-post-stop.sh"
-   /config/custom-post-stop.sh
+   echo "Executing /scripts/transmission-post-stop.sh"
+   /scripts/transmission-post-stop.sh
+   echo "/scripts/transmission-post-stop.sh returned $?"
 fi
