977fd4984eb98ae4148c9ac0e9a85a6f5654a59d
himred <himred@gasmi.net>
2018-02-08 10:27:24 +0100

    Update stop.sh

--- transmission/stop.sh
+++ transmission/stop.sh
@@ -1,3 +1,19 @@
 #! /bin/sh
 
+# If transmission-pre-stop.sh exists, run it
+if [ -x /scripts/transmission-pre-stop.sh ]
+then
+   echo "Executing /scripts/transmission-pre-stop.sh"
+   /scripts/transmission-pre-stop.sh
+   echo "/scripts/transmission-pre-stop.sh returned $?"
+fi
+
 kill $(pidof transmission-daemon)
+
+# If transmission-post-stop.sh exists, run it
+if [ -x /scripts/transmission-post-stop.sh ]
+then
+   echo "Executing /scripts/transmission-post-stop.sh"
+   /scripts/transmission-post-stop.sh
+   echo "/scripts/transmission-post-stop.sh returned $?"
+fi
