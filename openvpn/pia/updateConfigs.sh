d302efa3a4be210cab53907c362d999b16aee3bf
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2018-07-10 02:58:06 +0200

    PIA modified update script

--- openvpn/pia/updateConfigs.sh
+++ openvpn/pia/updateConfigs.sh
@@ -8,17 +8,42 @@ cd "${0%/*}"
 # Delete everything (not this script though)
 find . ! -name '*.sh' -delete
 
-# Get updated configuration zip
-curl -kL https://www.privateinternetaccess.com/openvpn/openvpn.zip -o openvpn.zip \
-  && unzip -j openvpn.zip && rm openvpn.zip
+baseURL="https://www.privateinternetaccess.com/openvpn/openvpn"
+extension=".zip"
+declare -a configsURLs=(    "" "-strong" "-tcp" "-strong-tcp" "-ip")
+declare -a configsFolders=( "" "strong"  "tcp"  "tcp-strong"  "ip")
 
-# Ensure linux line endings
-dos2unix *
+# warning: keeping folder name "tcp-strong" for legacy reasons, but the url is "strong-tcp".
 
-# Update configs with correct paths
-sed -i "s/ca ca\.rsa\.2048\.crt/ca \/etc\/openvpn\/pia\/ca\.rsa\.2048\.crt/" *.ovpn
-sed -i "s/crl-verify crl\.rsa\.2048\.pem/crl-verify \/etc\/openvpn\/pia\/crl\.rsa\.2048\.pem/" *.ovpn
-sed -i "s/auth-user-pass/auth-user-pass \/config\/openvpn-credentials.txt/" *.ovpn
+numberOfConfigTypes=${#configsURLs[@]}
+
+for (( i=1; i<${numberOfConfigTypes}+1; i++ ));
+do
+  requestURL="$baseURL${configsURLs[$i-1]}$extension"
+  if [ ! -z "${configsFolders[$i-1]}" ]
+  then
+    mkdir -p ${configsFolders[$i-1]} && cd ${configsFolders[$i-1]}
+  fi
+  curl -kL $requestURL -o openvpn.zip \
+    && unzip -j openvpn.zip && rm openvpn.zip
+
+  # Ensure linux line endings
+  dos2unix *
+
+  # Update configs with correct paths
+  folderNameWithEscapedSlash=""
+  if [ ! -z "${configsFolders[$i-1]}" ]
+  then
+    folderNameWithEscapedSlash="${configsFolders[$i-1]}\/"
+  fi
+  sed -i "s/auth-user-pass/auth-user-pass \/config\/openvpn-credentials.txt/" *.ovpn
+  sed -i "s/ca ca\.rsa\.\([0-9]*\)\.crt/ca \/etc\/openvpn\/pia\/${folderNameWithEscapedSlash}ca\.rsa\.\1\.crt/" *.ovpn
+  sed -i "s/crl-verify crl\.rsa\.\([0-9]*\)\.pem/crl-verify \/etc\/openvpn\/pia\/${folderNameWithEscapedSlash}crl\.rsa\.\1\.pem/" *.ovpn
+  if [ ! -z "${configsFolders[$i-1]}" ]
+  then
+    cd ..
+  fi
+done
 
 # Create symlink for default.ovpn
 ln -s Netherlands.ovpn default.ovpn
