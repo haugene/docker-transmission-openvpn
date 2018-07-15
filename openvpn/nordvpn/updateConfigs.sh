3870eb89ce6ade68027b87029c0ae2cda7585659
echaupynx <33941693+echaupynx@users.noreply.github.com>
2018-07-15 16:21:57 +0200

    changed options to updateConfigs.sh, also changed all Nordvpn .ovpn files

--- openvpn/nordvpn/updateConfigs.sh
+++ openvpn/nordvpn/updateConfigs.sh
@@ -12,16 +12,16 @@ find . ! -name '*.sh' -delete
 curl -kL https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip -o openvpn.zip \
   && unzip -j openvpn.zip && rm openvpn.zip
 
-#move files from folder ovpn_tcp and ovpn_udp, then remove the directory
-#mv ./ovpn_tcp/* ./
-#mv ./ovpn_udp/* ./
-#rmdir ovpn_tcp ovpn_udp
-
 # Ensure linux line endings
 #dos2unix *
 
-# Update configs with correct paths
-sed -i "s/auth-user-pass/auth-user-pass \/config\/openvpn-credentials.txt/" *.ovpn
+# Update configs with correct options
+sed -i 's=auth-user-pass=auth-user-pass /config/openvpn-credentials.txt=g' *.ovpn
+sed -i 's/ping 15/inactive 3600\
+ping 10/g' *.ovpn
+
+sed -i 's/ping-restart 0/ping-exit 60/g' *.ovpn
+sed -i 's/ping-timer-rem//g' *.ovpn
 
 # Create symlink for default.ovpn
 ln -s nl8.nordvpn.com.tcp.ovpn default.ovpn
