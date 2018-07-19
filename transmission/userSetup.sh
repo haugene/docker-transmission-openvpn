4db9d91957a4c4e303d64ea0b60b4f76db6429c2
dcrdev <development-K9RvgheM1OmXW9pm@dcrdev.com>
2018-07-19 19:29:08 +0100

    Refactor global permissions logic / add new directive to alpine/arm DockerFile

--- transmission/userSetup.sh
+++ transmission/userSetup.sh
@@ -4,36 +4,34 @@
 
 RUN_AS=root
 
-TEMP_TRAN_DOWNLOAD_DIR=${TRANSMISSION_DOWNLOAD_DIR}
-TEMP_TRAN_INCOMPLETE_DIR=${TRANSMISSION_INCOMPLETE_DIR}
-TEMP_TRAN_WATCH_DIR=${TRANSMISSION_WATCH_DIR}
-
-if ! [ "$GLOBAL_APPLY_PERMISSIONS" = true ] ; then
-    unset TEMP_TRAN_DOWNLOAD_DIR
-    unset TEMP_TRAN_INCOMPLETE_DIR
-    unset TEMP_TRAN_WATCH_DIR
-fi
-
 if [ -n "$PUID" ] && [ ! "$(id -u root)" -eq "$PUID" ]; then
     RUN_AS=abc
     if [ ! "$(id -u ${RUN_AS})" -eq "$PUID" ]; then usermod -o -u "$PUID" ${RUN_AS} ; fi
     if [ ! "$(id -g ${RUN_AS})" -eq "$PGID" ]; then groupmod -o -g "$PGID" ${RUN_AS} ; fi
 
-    echo "Setting owner for transmission paths to ${PUID}:${PGID}"
+    echo "Enforcing ownership on transmission config directories"
     chown -R ${RUN_AS}:${RUN_AS} \
         /config \
-        ${TRANSMISSION_HOME} \
-        ${TEMP_TRAN_DOWNLOAD_DIR} \
-        ${TEMP_TRAN_INCOMPLETE_DIR} \
-        ${TEMP_TRAN_WATCH_DIR}
+        ${TRANSMISSION_HOME}
         
-    echo "Setting permission for files (644) and directories (755)"
+    echo "Applying permissions to transmission config directories"
     chmod -R go=rX,u=rwX \
         /config \
-        ${TRANSMISSION_HOME} \
-        ${TEMP_TRAN_DOWNLOAD_DIR} \
-        ${TEMP_TRAN_INCOMPLETE_DIR} \
-        ${TEMP_TRAN_WATCH_DIR}    
+        ${TRANSMISSION_HOME}
+
+    if [ "$GLOBAL_APPLY_PERMISSIONS" = true ] ; then
+	echo "Setting owner for transmission paths to ${PUID}:${PGID}"
+        chown -R ${RUN_AS}:${RUN_AS} \
+            ${TRANSMISSION_DOWNLOAD_DIR} \
+            ${TRANSMISSION_INCOMPLETE_DIR} \
+            ${TRANSMISSION_WATCH_DIR}
+
+	echo "Setting permission for files (644) and directories (755)"
+        chmod -R go=rX,u=rwX \
+            ${TRANSMISSION_DOWNLOAD_DIR} \
+            ${TRANSMISSION_INCOMPLETE_DIR} \
+            ${TRANSMISSION_WATCH_DIR}
+    fi
 fi
 
 echo "
