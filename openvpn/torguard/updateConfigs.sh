c278b4971e81921a668bf8f6ac94f3790efe18d7
Alex <alexandre-del-bigio@alain-bensoussan.com>
2019-07-05 09:42:31 +0200

    Update TorGuard updateConfigs.sh and ovpn files

--- openvpn/torguard/updateConfigs.sh
+++ openvpn/torguard/updateConfigs.sh
@@ -14,12 +14,18 @@ curl -L https://torguard.net/downloads/OpenVPN-UDP.zip -o OpenVPN-UDP.zip \
 
 # Remove TorGuard prefix of config files and ensure linux line endings
 rename 's/^TorGuard\.//' *.ovpn
-dos2unix *
+
+# Convert CRLF to Linux
+sed -i "s/\r$//" *.ovpn
 
 # Update configs with correct paths
 sed -i "s/ca ca.crt/ca \/etc\/openvpn\/torguard\/ca.crt/" *.ovpn
 sed -i "s/tls-auth ta.key/tls-auth \/etc\/openvpn\/torguard\/ta.key/" *.ovpn
 sed -i "s/auth-user-pass/auth-user-pass \/config\/openvpn-credentials.txt/" *.ovpn
+
+# Remove incompatible directives
+sed -i "s/^block-outside-dns/# block-outside-dns/" *.ovpn
+sed -i "s/^keepalive/# keepalive/" *.ovpn
 
 # Create symlink for default.ovpn
 ln -s Netherlands.ovpn default.ovpn
