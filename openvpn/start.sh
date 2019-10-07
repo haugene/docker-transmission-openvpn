ceeae6d0e463831be97a174b82ce8e1579fb0210
Andrea Bonini <Gatto@GattoMac.local>
2019-10-07 23:04:21 +0200

    added vpnbook password grabber to start.sh

--- openvpn/start.sh
+++ openvpn/start.sh
@@ -57,6 +57,21 @@ elif [[ "${OPENVPN_PROVIDER^^}" = "FREEVPN" ]]
 then
     FREEVPN_DOMAIN=${OPENVPN_CONFIG%%-*}
     export OPENVPN_PASSWORD=$(curl -s https://freevpn.${FREEVPN_DOMAIN:-"be"}/accounts/ | grep Password |  sed s/"^.*Password\:.... "/""/g | sed s/"<.*"/""/g)
+elif [[ "${OPENVPN_PROVIDER^^}" = "VPNBOOK" ]]
+then
+    pwd_url=$(curl -s "https://www.vpnbook.com/freevpn" | grep -m2 "Password:" | tail -n1 | cut -d \" -f2)
+    curl -s -X POST --header "apikey: 5a64d478-9c89-43d8-88e3-c65de9999580" \
+      -F "url=https://www.vpnbook.com/${pwd_url}" \
+      -F 'language=eng' \
+      -F 'isOverlayRequired=true' \
+      -F 'FileType=.Auto' \
+      -F 'IsCreateSearchablePDF=false' \
+      -F 'isSearchablePdfHideTextLayer=true' \
+      -F 'scale=true' \
+      -F 'detectOrientation=false' \
+      -F 'isTable=false' \
+      "https://api.ocr.space/parse/image" -o /tmp/vpnbook_pwd
+    export OPENVPN_PASSWORD=$(cat /tmp/vpnbook_pwd  | awk -F',' '{ print $1 }' | awk -F':' '{print $NF}' | tr -d '"')
 fi
 
 if [[ -n "${OPENVPN_CONFIG-}" ]]; then
