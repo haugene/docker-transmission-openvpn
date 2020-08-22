26fc8843fb9f09180063e350a86f985885bbe6e3
Kristian Haugene <kristian@haugene.net>
2020-08-22 15:56:12 +0200

    Better logging

--- transmission/updateSettings.py
+++ transmission/updateSettings.py
@@ -35,10 +35,10 @@ if not os.path.isfile(default_settings):
 # Define which file to base the config on
 if os.path.isfile(transmission_settings):
     configuration_baseline = transmission_settings
+    print('Using existing settings.json for Transmission', transmission_settings)
 else:
     configuration_baseline = default_settings
-
-print('Using config baseline', configuration_baseline)
+    print('Generating settings.json for Transmission from environment and defaults', default_settings)
 
 # Read config base
 with open(configuration_baseline, 'r') as input_file:
