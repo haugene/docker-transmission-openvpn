5fad7c4697f4a75c228d3a2f5f87757593f68bc5
RickyXwang <896790683@qq.com>
2020-09-03 22:18:53 -0400

    Revert "Update tunnelDown.sh"
    This reverts commit 5da5af3cd2b457fd337cdf329839d0008b926a2d.

--- openvpn/tunnelDown.sh
+++ openvpn/tunnelDown.sh
@@ -2,21 +2,3 @@
 
 /etc/transmission/stop.sh
 [[ ! -f /opt/tinyproxy/stop.sh ]] || /opt/tinyproxy/stop.sh
-
-
-# Attempt to restart TUN
-
-INTERFACE=$(ls /sys/class/net | grep tun)
-ISINTERFACE=$?
-
-if [[ ${ISINTERFACE} -ne 0 ]]
-then
-        echo "TUN Interface not found"
-        exit 1
-fi
-
-ip link set ${INTERFACE} down
-sleep 1
-ip link set ${INTERFACE} up
-
-exit 0
