00d478bd5bb9ae88bf3a7770dbf1b03714768349
Kristian Haugene <kristian@haugene.net>
2020-11-04 03:27:27 +0100

    Add script for fetching configs on start for IPVanish #1456

--- openvpn/modify-openvpn-config.sh
+++ openvpn/modify-openvpn-config.sh
@@ -13,10 +13,18 @@ CONFIG=$1
 # But we should try to add sensible defaults, a way to disable it, and alternative implementations as needed.
 
 CONFIG_MOD_USERPASS=${CONFIG_MOD_USERPASS:-"1"}
+CONFIG_MOD_USERPASS=${CONFIG_MOD_CA_CERTS:-"1"}
 
 
 ## Option 1 - Change the auth-user-pass line to point to credentials file
 if [[ $CONFIG_MOD_USERPASS == "1" ]]; then
     [[ "${DEBUG}" == "true" ]] && echo "Point auth-user-pass option to the username/password file"
     sed -i "s#auth-user-pass#auth-user-pass /config/openvpn-credentials.txt#g" "$CONFIG"
+fi
+
+## Option 2 - Change the ca certificate path to point relative to the provider home
+if [[ $CONFIG_MOD_USERPASS == "1" ]]; then
+    [[ "${DEBUG}" == "true" ]] && echo "Change ca certificate path"
+    config_directory=$(dirname "$CONFIG")
+    sed -i "s#ca #ca $config_directory/#g" "$CONFIG"
 fi
