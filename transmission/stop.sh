e9e399c1884730e1ba528656c1a6ff6421d06d57
Clément Zrounba <clement.zrounba@hotmail.fr>
2020-11-14 17:00:54 +0100

    Add safeguard around transmission shutdown (fix #1513)

--- transmission/stop.sh
+++ transmission/stop.sh
@@ -8,14 +8,21 @@ then
    echo "/scripts/transmission-pre-stop.sh returned $?"
 fi
 
-echo "Sending kill signal to transmission-daemon"
+echo "Sending kill signal (SIGTERM) to transmission-daemon"
 PID=$(pidof transmission-daemon)
 kill $PID
+
 # Give transmission-daemon time to shut down
 for i in {1..10}; do
     ps -p "$PID" &> /dev/null || break
     sleep .2
 done
+
+# Check whether transmission-daemon is still running
+if ! ps -p "$PID" &> /dev/null; then
+    echo "Sending kill signal (SIGKILL) to transmission-daemon"
+    kill -9 $PID
+fi
 
 # If transmission-post-stop.sh exists, run it
 if [[ -x /scripts/transmission-post-stop.sh ]]
