1cabf9a424f3b3b7ba520377da9bd8b4c39ca83f
Clément Zrounba <clement.zrounba@hotmail.fr>
2020-11-16 23:50:32 +0100

    Make hard-coded transmission timeout into a variable
    * Set a default timeout value of 10 (from 2)
    * Set the default timeout periodic check to .5 (from .2)
    * Report waiting status and success in the log

--- transmission/stop.sh
+++ transmission/stop.sh
@@ -12,16 +12,21 @@ echo "Sending kill signal (SIGTERM) to transmission-daemon"
 PID=$(pidof transmission-daemon)
 kill $PID
 
-# Give transmission-daemon time to shut down
-for i in {1..10}; do
+# Give transmission-daemon some time to shut down
+TRANSMISSION_TIMEOUT_SEC=10
+TIMEOUT_NITER=$((2 * $TRANSMISSION_TIMEOUT_SEC))
+for i in $(seq $TIMEOUT_NITER); do
     ps -p "$PID" &> /dev/null || break
-    sleep .2
+    [[ $i == 1 ]] && echo "Waiting ${TRANSMISSION_TIMEOUT_SEC}sec for transmission-daemon to die"
+    sleep .5
 done
 
 # Check whether transmission-daemon is still running
 if ! ps -p "$PID" &> /dev/null; then
     echo "Sending kill signal (SIGKILL) to transmission-daemon"
     kill -9 $PID
+else
+    echo "Successfuly closed transmission-daemon"
 fi
 
 # If transmission-post-stop.sh exists, run it
