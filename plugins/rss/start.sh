643740c820d08b71e628223852a80bb20206f961
Kristian Haugene <kristian@haugene.net>
2020-12-06 21:54:17 +0100

    Brush up proxy and rss plugin images, build for multiarch support #1483

--- plugins/rss/start.sh
+++ plugins/rss/start.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 
-# Source our persisted env variables from container startup
-dockerize -template /etc/transmission-rss/environment-variables.tmpl:/etc/transmission-rss/environment-variables.sh
-. /etc/transmission-rss/environment-variables.sh
+#
+# This script tries to figure out how the container is configured.
+# The user has two options:
+# 1. Mount a custom config file to be used
+# 2. Use the built in template that supports one feed with regex filter
+#
 
-if [ -z "${RSS_URL}" ] || [ "${RSS_URL}" = "**None**" ] ; then
- echo "NO RSS URL CONFIGURED, IGNORING"
+if [ -f /etc/transmission-rss.conf ] ; then
+  echo "Found mounted /etc/transmission-rss.conf file"
+elif [ -z "${RSS_URL}" ] || [ "${RSS_URL}" = "**None**" ] ; then
+  echo "Error: No config is mounted and RSS_URL is not defined."
+  echo "Have no config to start from. Exit with error code."
+  exit 1
 else
+  # Configure plugin based on template. Use sed to insert 
+    sed "s^url: placeholder^url: $RSS_URL^" < /etc/transmission-rss/transmission-rss.tmpl \
+    | sed "s^download_path: placeholder^download_path: $TRANSMISSION_DOWNLOAD_DIR^" \
+    > /etc/transmission-rss.conf
+
   if [ -z "${RSS_REGEXP}" ] ; then
-  	sed -i 's/regexp:*//g' /etc/transmission-rss/transmission-rss.tmpl
+    sed -i '/regexp/d' /etc/transmission-rss.conf
+  else
+    sed -i "s#regexp: placeholder#regexp: $RSS_REGEXP#" /etc/transmission-rss.conf
   fi
-  dockerize -template /etc/transmission-rss/transmission-rss.tmpl:/etc/transmission-rss.conf
-  echo "STARTING RSS PLUGIN"
-  transmission-rss
 fi
+
+echo "Starting RSS plugin"
+cat /etc/transmission-rss.conf
+transmission-rss
