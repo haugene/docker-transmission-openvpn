53d3f05b7a1cad1a93f98fb99dec4fc9fee5ba6e
Kristian Haugene <kristian@haugene.net>
2020-12-12 00:39:44 +0100

    Escape special characters in rss-plugin input variables #1565

--- plugins/rss/start.sh
+++ plugins/rss/start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 # This script tries to figure out how the container is configured.
@@ -14,18 +14,25 @@ elif [ -z "${RSS_URL}" ] || [ "${RSS_URL}" = "**None**" ] ; then
   echo "Have no config to start from. Exit with error code."
   exit 1
 else
+  # The RSS url can (and usually does) contain special chars. We need to escape them
+  rss_url_esc=$(printf '%q' "$RSS_URL")
+  rss_regex_esc=$(printf '%q' "$RSS_REGEXP")
+
   # Configure plugin based on template. Use sed to insert 
-    sed "s^url: placeholder^url: $RSS_URL^" < /etc/transmission-rss/transmission-rss.tmpl \
+    sed "s^url: placeholder^url: $rss_url_esc^" < /etc/transmission-rss/transmission-rss.tmpl \
     | sed "s^download_path: placeholder^download_path: $TRANSMISSION_DOWNLOAD_DIR^" \
     > /etc/transmission-rss.conf
 
   if [ -z "${RSS_REGEXP}" ] ; then
     sed -i '/regexp/d' /etc/transmission-rss.conf
   else
-    sed -i "s#regexp: placeholder#regexp: $RSS_REGEXP#" /etc/transmission-rss.conf
+    sed -i "s#regexp: placeholder#regexp: $rss_regex_esc#" /etc/transmission-rss.conf
   fi
 fi
 
-echo "Starting RSS plugin"
+echo "Starting RSS plugin with the following config:"
+echo
 cat /etc/transmission-rss.conf
+
+echo
 transmission-rss
