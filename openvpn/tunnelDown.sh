b37fa164498da41bbd45291d8dfe5d665727bfec
Xenov <62152036+Xenov-X@users.noreply.github.com>
2020-12-19 11:48:06 +0000

    Update tunnelDown.sh

--- openvpn/tunnelDown.sh
+++ openvpn/tunnelDown.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/share/openrc/support/openvpn/down.sh
 /etc/transmission/stop.sh
+/usr/share/openrc/support/openvpn/down.sh
 [[ ! -f /opt/tinyproxy/stop.sh ]] || /opt/tinyproxy/stop.sh
