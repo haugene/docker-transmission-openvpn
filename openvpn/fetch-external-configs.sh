4622f5fb154341783b6806f27aeed61824f7eb0a
Kristian Haugene <kristian@haugene.net>
2021-03-18 00:22:14 +0100

    Add support for using git to fetch configs instead of curl

--- openvpn/fetch-external-configs.sh
+++ openvpn/fetch-external-configs.sh
@@ -45,6 +45,38 @@ if [[ "${VPN_CONFIG_SOURCE_TYPE}" == "github_zip" ]]; then
 
   exit 0
 
+elif [[ "${VPN_CONFIG_SOURCE_TYPE}" == "github_clone" ]]; then
+  GITHUB_CONFIG_REPO_URL="https://github.com/${GITHUB_CONFIG_SOURCE_REPO}.git"
+  config_repo=/tmp/config-repo
+
+  echo "Will get configs from ${GITHUB_CONFIG_REPO_URL}"
+  # Check if git repo exists and clone or pull based on that
+  if [[ -d ${config_repo} ]]; then
+    echo "Repository is already cloned, checking for update"
+    cd ${config_repo}
+    git pull
+    git checkout "${GITHUB_CONFIG_SOURCE_REVISION}"
+  else
+    echo "Cloning ${GITHUB_CONFIG_REPO_URL} into ${config_repo}"
+    git clone "${GITHUB_CONFIG_REPO_URL}" ${config_repo}
+    cd ${config_repo}
+    git checkout "${GITHUB_CONFIG_SOURCE_REVISION}"
+  fi
+
+  # Find the specified provider folder. Should be under <tmpDir>/<some-root-folder>/openvpn/<provider>
+  provider_configs=$(find "${config_repo}"/openvpn -type d -name "${VPN_PROVIDER}")
+  if [[ -z "${provider_configs}" ]]; then
+    echo "ERROR: Could not find any configs for provider ${VPN_PROVIDER^^} in downloaded configs"
+    exit 1
+  fi
+
+  # Replace current provider home folder with the one we've fetched
+  echo "Found configs for ${VPN_PROVIDER^^} in ${provider_configs}, will replace current content in ${VPN_PROVIDER_HOME}"
+  rm -r "${VPN_PROVIDER_HOME}"
+  cp -r "${provider_configs}" "${VPN_PROVIDER_HOME}"
+
+  exit 0
+
 else
     "ERROR: VPN config source type ${VPN_CONFIG_SOURCE_TYPE} does not exist..."
     exit 1
