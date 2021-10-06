553b560d46e8f7d873d32681106f5d9c551c25a0
Patrick Kishino <patrick.a.kishino@gmail.com>
2021-10-06 16:09:24 +0900

    TunnelDown restores resolv.conf if backup exists"

--- openvpn/tunnelDown.sh
+++ openvpn/tunnelDown.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#tunnel is down, restore resolv.conf to previous version
+if ls /etc/resolv.conf-*.sv 1> /dev/null 2>&1; then
+    cp /etc/resolv.conf-*.sv /etc/resolv.conf
+    echo "resolv.conf was restored"
+else
+    echo "resolv.conf backup not found, could not restore"
+fi
 
 /etc/transmission/stop.sh
 [[ -f /opt/privoxy/stop.sh ]] && /opt/privoxy/stop.sh
