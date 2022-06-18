cca5313c48b0b7d3bd653cc3f48d4c7745516a58
Derek Gordon <derek@derekgordon.com>
2022-06-18 18:08:12 -0400

    Update configure-openvpn.sh

--- openvpn/ovpn/configure-openvpn.sh
+++ openvpn/ovpn/configure-openvpn.sh
@@ -11,6 +11,8 @@ export OVPN_PROTOCOL
 OVPN_COUNTRY=$OVPN_COUNTRY
 export OVPN_COUNTRY
 
+OVPN_CITY=$OVPN_CITY
+export OVPN_CITY
 
 if [ -z "$VPN_PROVIDER_HOME" ]; then
     echo "ERROR: Need to have VPN_PROVIDER_HOME set to call this script" && exit 1
@@ -31,8 +33,10 @@ validate_options () {
 
 cd /etc/openvpn/ovpn
 
-pattern=$OVPN_CONNECTION.$OVPN_COUNTRY.*.$OVPN_PROTOCOL
-OPENVPN_CONFIG=$(ls $pattern | shuf | head -n1)
+#pattern=$OVPN_CONNECTION.$OVPN_COUNTRY.$OVPN_CITY.$OVPN_PROTOCOL
+#OPENVPN_CONFIG=$(ls $pattern | shuf | head -n1)
+
+OPENVPN_CONFIG=$OVPN_CONNECTION.$OVPN_COUNTRY.$OVPN_CITY.$OVPN_PROTOCOL
 
 #if [[ -n "$OPENVPN_CONFIG" ]]; then 
 #		ln -sf OPENVPN_CONFIG "$VPN_PROVIDER_HOME"/default.ovpn
