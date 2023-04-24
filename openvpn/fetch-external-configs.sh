ae3d2a6999c1c2e162393c0ea3d75171e600d259
ksurl <ksurl@users.noreply.github.com>
2023-04-24 03:41:06 +0000

    cleanup vpn config clone

--- openvpn/fetch-external-configs.sh
+++ openvpn/fetch-external-configs.sh
@@ -18,15 +18,19 @@ git config --global --add safe.directory "${config_repo}"
 echo "Will get configs from ${GITHUB_CONFIG_REPO_URL}"
 # Check if git repo exists and clone or pull based on that
 if [[ -d ${config_repo} ]]; then
-  echo "Repository is already cloned, checking for update"
-  cd ${config_repo}
-  git pull
-  git checkout "${GITHUB_CONFIG_SOURCE_REVISION}"
+  GITHUB_CONFIG_SOURCE_LOCAL=$(git -C "${config_repo}" remote -v | head -1 | awk '{print $2}' | sed -e 's/https:\/\/github.com\///' -e 's/.git//')
+  if [ "$GITHUB_CONFIG_SOURCE_LOCAL" == "$GITHUB_CONFIG_SOURCE_REPO" ]; then
+    echo "Repository is already cloned, checking for update"
+    git -C "${config_repo}" pull
+    git -C "${config_repo}" checkout "${GITHUB_CONFIG_SOURCE_REVISION}"
+  else
+    echo "Cloning ${GITHUB_CONFIG_REPO_URL} into ${config_repo}"
+    config_repo_old="${config_repo}" + "_old"
+    mv "${config_repo}" "${config_repo_old}"
+    git clone -b "${GITHUB_CONFIG_SOURCE_REVISION}" "${GITHUB_CONFIG_REPO_URL}" "${config_repo}"
 else
   echo "Cloning ${GITHUB_CONFIG_REPO_URL} into ${config_repo}"
-  git clone --depth 1 -b "${GITHUB_CONFIG_SOURCE_REVISION}" "${GITHUB_CONFIG_REPO_URL}" ${config_repo}
-  cd ${config_repo}
-  git checkout "${GITHUB_CONFIG_SOURCE_REVISION}"
+  git clone -b "${GITHUB_CONFIG_SOURCE_REVISION}" "${GITHUB_CONFIG_REPO_URL}" "${config_repo}"
 fi
 
 # Find the specified provider folder. Should be under <tmpDir>/<some-root-folder>/openvpn/<provider>
