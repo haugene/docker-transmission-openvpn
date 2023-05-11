21694981edb01a6e0e032f24906b96dc6c580947
HeavyGee <133152184+heavygee@users.noreply.github.com>
2023-05-11 13:01:08 +0100

    Privoxy eth0 fixes, healthcheck comparison tweak and start.sh update (#2610)
    * by checking for existence of default 127 address,healthcheck will never effectively run, as the non-default eth0 ip will never be matched
    
    * the comparison was not trimming off the port, thus healthcheck was always going to fail or denote a change when there may have been none
    
    * simplified the comparison (thanks @edgd1er )

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -59,7 +59,7 @@ if [[ ${WEBPROXY_ENABLED} =~ [yY][eE]?[Ss]?|[tT][Rr][Uu][eE] ]]; then
   if [[ ${PROXY} -eq 0 ]]; then
     echo "Privoxy warning: process was stopped, restarting."
   fi
-    proxy_ip=$(grep -oP "(?<=^listen-address).*$" /etc/privoxy/config | sed 's/ //g')
+    proxy_ip=$(grep -oP "(?<=^listen-address )[0-9\.]+" /etc/privoxy/config)
     cont_ip=$(ip -j a show dev eth0 | jq -r .[].addr_info[].local)
     if [[ ${proxy_ip} != ${cont_ip} ]]; then
       echo "Privoxy error: container ip (${cont_ip} has changed: privoxy listening to ${proxy_ip}, restarting privoxy."
